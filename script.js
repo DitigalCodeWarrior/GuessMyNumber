b8f491ea11b5378b57ac9899a4144fcb1714a22c
CodeRanger2023 <codehunter2023@gmail.com>
2024-01-27 14:09:03 +0200

    implement reset functionality

--- script.js
+++ script.js
@@ -9,9 +9,8 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener(`click`, function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,6 +23,7 @@ document.querySelector('.check').addEventListener(`click`, function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = `🎉 Correct number!`;
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -49,4 +49,15 @@ document.querySelector('.check').addEventListener(`click`, function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = `Start guessing...`;
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
 });
